7046f0a0de72f537c4194edf7c7717be4ceba07d
Zearom <zearom@me.com>
2015-05-08 23:46:03 +0200

    PCARS Dev Start

---
+++ ReadPCarsApi/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim pcarsApiFile As System.IO.MemoryMappedFiles.MemoryMappedFile = System.IO.MemoryMappedFiles.MemoryMappedFile.OpenExisting("$pcars$")
+        Dim physicObject As IO.MemoryMappedFiles.MemoryMappedViewStream = pcarsApiFile.CreateViewStream()
+        Dim reader As BinaryReader = New BinaryReader(physicObject)
+        Dim bytes As Byte() = reader.ReadBytes(1)
+    End Sub
+
+End Module
